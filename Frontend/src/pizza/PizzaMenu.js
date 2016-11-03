d7cf80598747c501c1aa7ab04c42b7c914e6a1cd
Andrew Gomenyuk <platinium9889@gmail.com>
2016-11-03 23:47:42 +0200

    All JS done.

---
+++ Frontend/src/pizza/PizzaMenu.js
@@ -0,0 +1,83 @@
+/**
+ * Created by chaika on 02.02.16.
+ */
+var Templates = require('../Templates');
+var PizzaCart = require('./PizzaCart');
+var Pizza_List = require('../Pizza_List');
+
+//this are filters
+var Filters = {
+    filter_button_all_pizza: "",
+    filter_button_meat: "meat",
+    filter_button_pineapples: "pineapple",
+    filter_button_mushrooms: "mushroom",
+    filter_button_ocean: "ocean",
+    filter_button_tomato: "additional"
+};
+
+//HTML едемент куди будуть додаватися піци
+var $pizza_list = $("#pizza_list");
+//filter buttons
+var $filters = $(".pizza-filter-button");
+//pizza counter
+var $pizza_list_counter = $("#pizza-list-counter");
+
+$filters.click(function() { 
+    var id = $(this).attr("id");
+    $(".pizza-filter-button.active").removeClass("active");
+    $(this).addClass("active");
+    var filter = Filters[id];
+    filterPizza(filter);
+});
+
+function showPizzaList(list) {
+    //Очищаємо старі піци в кошику
+    $pizza_list.html("");
+
+    //Онволення однієї піци
+    function showOnePizza(pizza) {
+        var html_code = Templates.PizzaMenu_OneItem({pizza: pizza});
+
+        var $node = $(html_code);
+
+        $node.find(".buy-button-big").click(function(){
+            PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Big);
+        });
+        
+        $node.find(".buy-button-small").click(function(){
+            PizzaCart.addToCart(pizza, PizzaCart.PizzaSize.Small);
+        });
+
+        $pizza_list.append($node);
+    }
+
+    $pizza_list_counter.text(list.length);
+    list.forEach(showOnePizza);
+}
+
+function filterPizza(filter) {
+    //Масив куди потраплять піци які треба показати
+    var pizza_shown = [];
+
+    Pizza_List.forEach(function(pizza){
+        if(filter == "additional" && pizza.content['meat']) {
+            return;
+        }
+        if(!filter || pizza.content[filter]) {
+            pizza_shown.push(pizza);
+        }
+
+        //TODO: зробити фільтри
+    });
+
+    //Показати відфільтровані піци
+    showPizzaList(pizza_shown);
+}
+
+function initialiseMenu() {
+    //Показуємо усі піци
+    showPizzaList(Pizza_List)
+}
+
+exports.filterPizza = filterPizza;
+exports.initialiseMenu = initialiseMenu;
